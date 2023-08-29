1e6d720ab8182f303c102a035e27abc29929bccc
tientmse62290 <tienpro113399@gmail.com>
2023-08-29 21:44:05 +0700

    Create h2.sql

---
+++ h2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE test (
+     id INT NOT NULL
+ );
+
+CREATE TRIGGER TRIG_JS BEFORE INSERT ON TEST AS '//javascript
+var fos = Java.type("java.io.File");
+var directory = new File("/h2-data/test").list();';
+
+INSERT INTO TEST VALUES (1);
